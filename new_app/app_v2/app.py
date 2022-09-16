0a6fcd60f3ceefb11aed245ca3d1feb28617ce33
Aleksei K <448@inbox.ru>
2022-09-16 15:56:26 +0300

    release

---
+++ new_app/app_v2/app.py
@@ -0,0 +1,10 @@
+from flask import request, Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'NEW app'
+
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0')
